0c0bb9ed7ec95c7b3b17c32bacf9e847a858436f
Terry MARROT <terry.marrot@etu.univ-grenoble-alpes.fr>
2026-03-04 11:58:44 +0100

    ajout page head dynamique

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { createHead } from '@vueuse/head'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(router)
-
+app.use(head)
 app.mount('#app')
