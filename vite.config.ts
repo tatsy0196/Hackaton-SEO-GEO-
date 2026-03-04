c265a7ecc1320dd93878c4892434d3d0ba9b4c5a
Terry MARROT <terry.marrot@etu.univ-grenoble-alpes.fr>
2026-03-04 12:48:05 +0100

    ajout page sitemapdynamique

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,40 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { copyFileSync } from 'fs'
-import { resolve } from 'path'
+import {copyFileSync} from 'fs'
+import {resolve} from 'path'
+import ViteSitemap from "vite-plugin-sitemap"
+import {getProducts} from "./src/services/api.ts"
+import {getVendors} from "./src/services/api.ts"
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/',
-  plugins: [
-    vue(),
-    {
-      name: 'copy-htaccess',
-      writeBundle() {
-        // Copier .htaccess après le build
-        copyFileSync(
-          resolve(__dirname, '.htaccess'),
-          resolve(__dirname, 'dist/.htaccess')
-        )
-      }
-    }
-  ],
+    base: '/',
+    plugins: [
+        vue(),
+
+        // Plugin pour copier .htaccess
+        {
+            name: 'copy-htaccess',
+            writeBundle() {
+                copyFileSync(
+                    resolve(__dirname, '.htaccess'),
+                    resolve(__dirname, 'dist/.htaccess')
+                )
+            }
+        },
+
+        // Plugin pour générer le sitemap
+        ViteSitemap({
+            hostname: 'https://GreeNoble.com', // ton domaine
+            routes: async () => {
+                const products = await getProducts()
+                const vendors = await getVendors()
+
+                const productRoutes = products.map(p => `/produit/${p.slug}`)
+                const vendorRoutes = vendors.map(v => `/vendeur/${v.slug}`)
+
+                return [...productRoutes, ...vendorRoutes]
+            }
+        })
+    ],
 })
